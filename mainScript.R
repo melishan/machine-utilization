abf3e3c8403782eed2513d42cd9a0e3fe0e29c01
melishan <melishan@gmail.com>
2018-05-16 09:26:46 +0300

    list preparation

--- mainScript.R
+++ mainScript.R
@@ -24,3 +24,34 @@ head(machine, 10)
 #changing columns
 machine <- machine[,c(4,1,2,3)]
 head(machine, 10)
+
+#Lists
+summary(machine)
+RL1 <- machine[machine$Machine=="RL1",]
+summary(RL1)
+#we still see other machines too. We should keep machine as factor
+RL1$Machine <- factor(RL1$Machine)
+summary(RL1)
+
+#Construct a list
+#Character: Machine name
+#Vector: (min, mean, max) Utilization for the month (excluding uknown hours)
+#Logical: Has utilization ever fallen below 90%? TRUE/FALSE
+
+machine_stats_RL1 <- c(min(RL1$Utilization, na.rm = TRUE),
+                        mean(RL1$Utilization, na.rm = TRUE),
+                          max(RL1$Utilization, na.rm = TRUE))
+machine_stats_RL1
+
+length(which(RL1$Utilization < 0.9)) #Has utilization ever fallen below 90%?
+length(which(RL1$Utilization < 0.9)) > 0 #convert it to logical operatior
+
+
+machine_under_90 <- length(which(RL1$Utilization < 0.9)) > 0
+machine_under_90
+
+list_RL1 <- list("RL1", machine_stats_RL1, machine_under_90)
+list_RL1 #numbers of components show as double bracket
+
+
+
