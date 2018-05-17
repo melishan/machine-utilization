2fdc61a1197f3f7eebf894c6fc163ff870a60a40
melishan <melishan@gmail.com>
2018-05-17 09:36:02 +0300

    construction the list

--- mainScript.R
+++ mainScript.R
@@ -37,6 +37,9 @@ summary(RL1)
 #Character: Machine name
 #Vector: (min, mean, max) Utilization for the month (excluding uknown hours)
 #Logical: Has utilization ever fallen below 90%? TRUE/FALSE
+#Vector: All hours where utilization is unknown (NA's)
+#dataframe: dataframe: for this machine
+#Plot: for all machines
 
 machine_stats_RL1 <- c(min(RL1$Utilization, na.rm = TRUE),
                         mean(RL1$Utilization, na.rm = TRUE),
@@ -51,7 +54,56 @@ machine_under_90 <- length(which(RL1$Utilization < 0.9)) > 0
 machine_under_90
 
 list_RL1 <- list("RL1", machine_stats_RL1, machine_under_90)
-list_RL1 #numbers of components show as double bracket
+list_RL1 #names of components show as double bracket in lists
+names(list_RL1) <- c("Machine", "Stats","LowThreshold")
+list_RL1
+#Another way like with dataframes
+list_RL1 <- list(Machine="RL1", Stats=machine_stats_RL1,LowThreshold=machine_under_90)
+list_RL1
+
+#Extracting components of a list
+#three ways:
+#[] - will always return a list
+#[[]] - will always return the actual object
+#$ - sames as [[]] but prettier 
+
+list_RL1[2]  #it returns elemment of a vector
+list_RL1[[2]]  #it returns object not an element of list
+list_RL1$Stats #it returns object not an element of list
+
+typeof(list_RL1[2]) #returns list
+typeof(list_RL1[[2]]) #returns double 
+typeof(list_RL1$Stats) #returns double
 
 
+#how would you access the 3rd element of the vector (max utilization)
+list_RL1
+list_RL1[[2]][3] #to pick the spesific element of the vector.
+list_RL1$Stats[3] #to pick the spesific element of the vector.
 
+#adding and deleting list components
+list_RL1
+
+list_RL1[4] <- "New Information"
+
+#another way to add a component via $
+#Vector: All hours where utilization is unknown (NA's)
+RL1[is.na(RL1$Utilization), "PosixTime"]
+list_RL1$UnknownHours <- RL1[is.na(RL1$Utilization), "PosixTime"]
+list_RL1
+
+#remove a component from a list
+list_RL1[4] <- NULL
+list_RL1
+
+#!!!!Notice: numeraton has shifted
+list_RL1[[4]][3]
+
+#add another component
+#dataframe: for this machine
+
+list_RL1$data <- RL1
+list_RL1
+
+summary(list_RL1)
+str(list_RL1)
